b39c959f6d9d3f8a99225e3122edfbe2befa8d1f
someotherguy <someotherguy@gmail.com>
2018-10-25 18:24:27 -0700

    New Python crud

---
+++ find_first_duplicate.py
@@ -0,0 +1,58 @@
+
+# Notes: First thing I tried was doing this with lists since the incoming thing is a list. This is too slow because
+# lists are 0(n) search/membership in Python. Tried with Dicts, worked.
+# Sets are even more elegant because it forces uniqueness.
+# We don't really care about the index since the first dupe we find will be the earliest second occurrence.
+
+
+def first_duplicate(a):
+
+    # Strategy: Search the incoming list and use a temp set to see if you hit a dupe. When you do, return it.
+    # If you don't find any dupes return -1.
+
+    search_set = set()
+
+    for number_elem in a:
+
+        if number_elem in search_set:
+            return number_elem
+        else:
+            search_set.add(number_elem)
+
+    # If you can't find any dupes, return -1.
+    return -1
+
+
+first_test_case = first_duplicate([2, 1, 3, 5, 3, 2])
+print("First test from example: [2, 1, 3, 5, 3, 2], expected output: 3 -> " + str(first_test_case))
+
+second_test_case = first_duplicate([2, 4, 3, 5, 1])
+print("Second test from example: [2, 4, 3, 5, 1], expected output: -1 -> " + str(second_test_case))
+
+# Below is from: https://app.codesignal.com/interview-practice/task/pMvymcahZ8dY4g75q
+# Apparently a Google coding question.
+# Given an array a that contains only numbers in the range from 1 to a.length, find the first duplicate number for which the second occurrence has the minimal index. In other words, if there are more than 1 duplicated numbers, return the number for which the second occurrence has a smaller index than the second occurrence of the other number does. If there are no such elements, return -1.
+#
+# Example
+#
+# For a = [2, 1, 3, 5, 3, 2], the output should be
+# firstDuplicate(a) = 3.
+#
+# There are 2 duplicates: numbers 2 and 3. The second occurrence of 3 has a smaller index than the second occurrence of 2 does, so the answer is 3.
+#
+# For a = [2, 4, 3, 5, 1], the output should be
+# firstDuplicate(a) = -1.
+#
+# Input/Output
+#
+# [execution time limit] 4 seconds (py3)
+#
+# [input] array.integer a
+#
+# Guaranteed constraints:
+# 1 ≤ a.length ≤ 105,
+# 1 ≤ a[i] ≤ a.length.
+#
+# [output] integer
+#
+# The element in a that occurs in the array more than once and has the minimal index for its second occurrence. If there are no such elements, return -1.
---
+++ first_not_repeating_character.py
@@ -0,0 +1,73 @@
+def first_not_repeating_character(s):
+
+    # Strategy go through every character and add them to a dictionary. Key is char and value is the index.
+    # If you encounter it again, remove from dictionary and add it to a "ignore" set to skip further occurences.
+    # Once done, sort the dictionary by index and print the min, or if empty return "_"
+    #
+    # You could probably remove the set and just put the index at -1 or something and skip that to save a bit of space.
+
+    search_dict = {}
+    ignore_set = set()
+
+    for string_counter in range(len(s)):
+
+        # Check to see if we need to ignore the char cause it already had a repeat.
+        if s[string_counter] not in ignore_set:
+
+            # Check to see if it is a repeat, if so gotta remove it from search space and add to ignore set.
+            if s[string_counter] in search_dict:
+                del search_dict[s[string_counter]]
+                ignore_set.add(s[string_counter])
+            else:
+                # If it's not previously ignored or a repeat, it's new. Add it to the search space.
+                search_dict[s[string_counter]] = string_counter
+
+    # print("The min index is: " + str(the_answer) + " the character at that index is: " + s[the_answer] )
+    # print("The list of not repeats are: " + str(search_dict))
+
+    # Dictionaries equivocate to false if empty.
+    if search_dict:
+        return s[min(search_dict.values())]
+    else:
+        return "_"
+
+
+firstTestCase = first_not_repeating_character("abacabad")
+print ("The first test case tests \"abacabad\" and expected output is: 'c' -> " + firstTestCase)
+
+secondTestCase = first_not_repeating_character("abacabaabacaba")
+print ("The first test case tests \"abacabaabacaba\" and expected output is: '_' -> " + secondTestCase)
+
+
+# From here: https://app.codesignal.com/interview-practice/task/uX5iLwhc6L5ckSyNC
+# Apparently a question Amazon asks.
+# Note: Write a solution that only iterates over the string once and uses O(1) additional memory, since this is what you would be asked to do during a real interview.
+#
+# Given a string s, find and return the first instance of a non-repeating character in it. If there is no such character, return '_'.
+#
+# Example
+#
+# For s = "abacabad", the output should be
+# firstNotRepeatingCharacter(s) = 'c'.
+#
+# There are 2 non-repeating characters in the string: 'c' and 'd'. Return c since it appears in the string first.
+#
+# For s = "abacabaabacaba", the output should be
+# firstNotRepeatingCharacter(s) = '_'.
+#
+# There are no characters in this string that do not repeat.
+#
+# Input/Output
+#
+# [execution time limit] 4 seconds (py3)
+#
+# [input] string s
+#
+# A string that contains only lowercase English letters.
+#
+# Guaranteed constraints:
+# 1 ≤ s.length ≤ 105.
+#
+# [output] char
+#
+# The first non-repeating character in s, or '_' if there are no characters that do not repeat.
