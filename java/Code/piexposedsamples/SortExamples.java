72e5048082617869f0a721407389758ba6066012
Chris Lee <topagae@gmail.com>
2021-12-26 17:24:41 -0800

    Adding old Java code, people keep asking about multiple languages.

---
+++ java/Code/piexposedsamples/SortExamples.java
@@ -0,0 +1,14 @@
+package Code.piexposedsamples;
+
+import java.util.*;
+
+/**
+ * For reference, exercises with Sorting. Annotated for learning. I should've saved this from college.
+ *
+ * Note: To make this work in coder pad refactor the name of the class to "Solution" or w/e the web thing uses.
+ *
+ * @author Chris Lee
+ */
+public class SortExamples {
+
+}
